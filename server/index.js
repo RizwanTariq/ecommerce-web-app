5022ac82f6cc30742c3590d6c47daa65cf14f547
Rizwan Tariq <rizwantariq95@outlook.com>
2022-01-18 02:07:57 +0500

    configure server to use ES6 modules.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-require('dotenv').config()
-const products = require('./data/products')
-const app = require('express')() //Same as const app = express()
+import 'dotenv/config'
+import express from 'express'
+import products from './data/products.js'
+
+const app = express()
 app.get('/', (req, res) => {
   res.send('Api is running..')
 })
