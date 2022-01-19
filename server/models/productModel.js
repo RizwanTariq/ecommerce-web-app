9d0e3b6c6684b1ae143a075f1ea146fdeb037664
Rizwan Tariq <rizwantariq95@outlook.com>
2022-01-19 16:39:35 +0500

    Build models. (user, product, order)

---
+++ server/models/productModel.js
@@ -0,0 +1,75 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+//Review Schema
+const reviewSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+const productSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    avgRating: {
+      type: Boolean,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    reviews: [reviewSchema],
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('Product', productSchema)
