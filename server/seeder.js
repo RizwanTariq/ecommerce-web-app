67c31b76ef8a63175a6c3b82fe17ccebf6008325
Rizwan Tariq <rizwantariq95@outlook.com>
2022-01-19 22:49:12 +0500

    write seeder script for adding and removing sample data in db.

---
+++ server/seeder.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose'
+import dotEnv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import users from './data/users.js'
+import User from './models/userModel.js'
+import Product from './models/productModel.js'
+import Order from './models/orderModel.js'
+import products from './data/products.js'
+
+dotEnv.config()
+connectDB()
+
+const importData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    const createdUsers = await User.insertMany(users)
+    const adminUser = createdUsers.find((user) => user.isAdmin === true)
+    const sampleProducts = products.map((product) => ({
+      ...product,
+      user: adminUser,
+    }))
+
+    await Product.insertMany(sampleProducts)
+    console.log('Data Inserted'.green)
+    process.exit(0)
+  } catch (err) {
+    console.log(`${err.message}`.red)
+    process.exit(1)
+  }
+}
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    console.log('Data Destroyed'.green)
+    process.exit(0)
+  } catch (err) {
+    console.log(`${err.message}`.red)
+    process.exit(1)
+  }
+}
+
+if (process.argv[2] === '-D') {
+  destroyData()
+} else {
+  importData()
+}
