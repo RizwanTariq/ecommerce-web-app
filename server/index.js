c3eef9f2ffecf43b489433a24a1da08cd44ceebf
Rizwan Tariq <rizwantariq95@outlook.com>
2022-01-21 01:19:55 +0500

    implemented error middlewares.

--- server/index.js
+++ server/index.js
@@ -2,15 +2,20 @@ import 'colors'
 import 'dotenv/config' //Same as dotenv.config()
 import express from 'express'
 import connectDB from './config/db.js'
-
 import router from './routes/index.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 //Connect with DATABASE
 connectDB()
 
 const app = express()
 
+//Middlewares
 app.use('/', router)
+
+//error middlewares
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 const ENVIORNMENT = process.env.NODE_ENV
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -24,10 +24,11 @@ router.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
-    if (!product) {
-      res.status(404).json({ message: 'Product not found' })
-    } else {
+    if (product) {
       res.json(product)
+    } else {
+      res.status(404)
+      throw new Error('Product not found.')
     }
   })
 )
