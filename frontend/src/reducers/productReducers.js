873364e97cf7ec24117b9865f0ae2ce9bdf50a75
Rizwan Tariq <rizwantariq95@outlook.com>
2022-01-23 22:33:41 +0500

    product details reducer implemented.

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,15 +1,34 @@
-import { PRODUCT_LIST } from '../constants/productConstants'
+import { PRODUCT_DETAILS, PRODUCT_LIST } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST.REQUEST:
-      return { loading: true, products: [] }
+      return { loading: true, ...state }
 
     case PRODUCT_LIST.SUCCESS:
       return { loading: false, products: [...action.payload] }
 
     case PRODUCT_LIST.FAIL:
       return { loading: false, error: [...action.payload] }
+
+    default:
+      return state
+  }
+}
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS.REQUEST:
+      return { loading: true, ...state }
+
+    case PRODUCT_DETAILS.SUCCESS:
+      return { loading: false, product: { ...action.payload } }
+
+    case PRODUCT_DETAILS.FAIL:
+      return { loading: false, error: { ...action.payload } }
 
     default:
       return state
