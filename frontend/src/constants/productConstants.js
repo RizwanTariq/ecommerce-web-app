4e94cb80935e70a41a530ef8676dfbc71f5ddb28
Rizwan Tariq <rizwantariq95@outlook.com>
2022-01-23 22:33:04 +0500

    Product details constants added.

--- frontend/src/constants/productConstants.js
+++ frontend/src/constants/productConstants.js
@@ -9,3 +9,15 @@ export const PRODUCT_LIST = {
     return 'PRODUCT_LIST_FAIL'
   },
 }
+
+export const PRODUCT_DETAILS = {
+  get REQUEST() {
+    return 'PRODUCT_DETAILS_REQUEST'
+  },
+  get SUCCESS() {
+    return 'PRODUCT_DETAILS_SUCCESS'
+  },
+  get FAIL() {
+    return 'PRODUCT_DETAILS_FAIL'
+  },
+}
